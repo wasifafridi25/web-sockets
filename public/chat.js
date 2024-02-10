a1810cb7edd3890a4f5552079f0347fbaf7726f3
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2024-02-09 16:11:22 -0800

    Broadcasted to every other except itself

--- public/chat.js
+++ public/chat.js
@@ -5,7 +5,8 @@ const socket = io.connect("http://localhost:3000/");
 const message = document.getElementById("message"),
   handle = document.getElementById("handle"),
   btn = document.getElementById("send"),
-  output = document.getElementById("output");
+  output = document.getElementById("output"),
+  feedback = document.getElementById("feedback");
 
 //Emit events
 btn.addEventListener("click", () => {
@@ -15,7 +16,17 @@ btn.addEventListener("click", () => {
   });
 });
 
-//Listenfor events and ouput to the DOM
-socket.on('chat', (data) => {
-    output.innerHTML += '<p><strong>' + data.handle + ": " + '</strong>' + data.message + '</p>'
-})
+message.addEventListener("keypress", () => {
+  socket.emit("typing", handle.value);
+});
+
+//Listen for events and ouput to the DOM
+socket.on("chat", (data) => {
+  feedback.innerHTML = "";
+  output.innerHTML +=
+    "<p><strong>" + data.handle + ": " + "</strong>" + data.message + "</p>";
+});
+
+socket.on("typing", (data) => {
+  feedback.innerHTML = "<p>" + data + " is typing..." + "</p>";
+});
